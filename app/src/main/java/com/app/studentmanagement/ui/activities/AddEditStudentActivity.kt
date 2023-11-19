aa324a99bceeeb40bf355a7fc5b29e624a2bdaa3
Le Hoang Phuc <52100832@student.tdtu.edu.vn>
2023-11-19 20:04:22 +0700

    make all UI, not data

---
+++ app/src/main/java/com/app/studentmanagement/ui/activities/AddEditStudentActivity.kt
@@ -0,0 +1,12 @@
+package com.app.studentmanagement.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.studentmanagement.R
+
+class AddEditStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_student)
+    }
+}
---
+++ app/src/main/java/com/app/studentmanagement/ui/activities/DetailStudentInformationActivity.kt
@@ -0,0 +1,12 @@
+package com.app.studentmanagement.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.studentmanagement.R
+
+class DetailStudentInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_student_information)
+    }
+}
